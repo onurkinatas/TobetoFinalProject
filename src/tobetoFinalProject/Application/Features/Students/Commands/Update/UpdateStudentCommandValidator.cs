831f89ba5089e7c43ea15f5fa32d9aefd3ecd626
Ahmet-Akpolat <stepiem@outlook.com>
2024-01-18 23:10:37 +0300

    Student Validation Operation Added

--- src/tobetoFinalProject/Application/Features/Students/Commands/Update/UpdateStudentCommandValidator.cs
+++ src/tobetoFinalProject/Application/Features/Students/Commands/Update/UpdateStudentCommandValidator.cs
@@ -8,14 +8,24 @@ public class UpdateStudentCommandValidator : AbstractValidator<UpdateStudentComm
     {
         RuleFor(c => c.Id).NotEmpty();
         RuleFor(c => c.UserId).NotEmpty();
-        RuleFor(c => c.CityId).NotEmpty();
-        RuleFor(c => c.DistrictId).NotEmpty();
-        RuleFor(c => c.NationalIdentity).NotEmpty();
-        RuleFor(c => c.Phone).NotEmpty();
-        RuleFor(c => c.BirthDate).NotEmpty();
-        RuleFor(c => c.AddressDetail).NotEmpty();
-        RuleFor(c => c.Description).NotEmpty();
-        RuleFor(c => c.ProfilePhotoPath).NotEmpty();
-        RuleFor(c => c.Country).NotEmpty();
+
+        RuleFor(c => c.Phone)
+            .Matches(@"^\d{11}$").WithMessage("Telefon numarasý tam olarak11 rakamdan oluþmalýdýr.");
+
+        RuleFor(c => c.NationalIdentity)
+            .Matches(@"^\d{11}$").WithMessage("TC Kimlik Numarasý 11 rakamdan oluþmalýdýr.");
+
+        RuleFor(c => c.BirthDate)
+            .InclusiveBetween(DateTime.Today.AddYears(-35), DateTime.Today.AddYears(-18))
+            .WithMessage("Yaþ 18 ile 35 arasýnda olmalýdýr.");
+
+        RuleFor(c => c.Country)
+            .Matches(@"^[a-zA-ZðüþöçÝÐÜÞÖÇ]+$").WithMessage("Sadece harflerden oluþmalýdýr.")
+            .MinimumLength(2).WithMessage("Ülke 2 karakterden az olamaz.")
+            .MaximumLength(30).WithMessage("Ülke 30 karakterden çok olamaz.");
+
+        RuleFor(c => c.Description).MaximumLength(300).WithMessage("Hakkýmda En fazla 300 karakter olmalý");
+
+        RuleFor(c => c.AddressDetail).MaximumLength(200).WithMessage("Adres Detaylarý En fazla 200 karakter olmalý");
     }
 }
