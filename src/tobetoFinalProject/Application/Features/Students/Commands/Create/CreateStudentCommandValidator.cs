b7462f386b3d1bab69e8baa61f00c86b585b7233
onurkinatas <kntsonur3428@gmail.com>
2024-01-18 19:29:49 +0300

    CreateStudentCommandValidator is validation added.

--- src/tobetoFinalProject/Application/Features/Students/Commands/Create/CreateStudentCommandValidator.cs
+++ src/tobetoFinalProject/Application/Features/Students/Commands/Create/CreateStudentCommandValidator.cs
@@ -6,5 +6,12 @@ public class CreateStudentCommandValidator : AbstractValidator<CreateStudentComm
 {
     public CreateStudentCommandValidator()
     {
+        RuleFor(x => x.FirstName).NotEmpty().WithMessage("Öðrenci adý boþ olamaz.");
+
+        RuleFor(x => x.LastName).NotEmpty().WithMessage("Öðrenci soyadý boþ olamaz.");
+
+        RuleFor(x => x.Email).NotEmpty().EmailAddress().WithMessage("Geçerli bir e-posta adresi giriniz.");
+
+        RuleFor(x => x.Password).NotEmpty().MinimumLength(6).WithMessage("Þifre en az 6 karakter uzunluðunda olmalýdýr.");
     }
 }
