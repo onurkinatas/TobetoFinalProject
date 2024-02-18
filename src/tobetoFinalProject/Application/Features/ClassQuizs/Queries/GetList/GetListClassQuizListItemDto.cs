dca21ff5ec785b3c30840c12724c55544065220d
Koyuncu <130415130+koyuncu5362@users.noreply.github.com>
2024-02-19 00:45:38 +0300

    Quiz Operations Completed

--- src/tobetoFinalProject/Application/Features/ClassQuizs/Queries/GetList/GetListClassQuizListItemDto.cs
+++ src/tobetoFinalProject/Application/Features/ClassQuizs/Queries/GetList/GetListClassQuizListItemDto.cs
@@ -1,3 +1,4 @@
+using Application.Features.Quizs.Queries.GetById;
 using Core.Application.Dtos;
 
 namespace Application.Features.ClassQuizs.Queries.GetList;
@@ -5,6 +6,6 @@ namespace Application.Features.ClassQuizs.Queries.GetList;
 public class GetListClassQuizListItemDto : IDto
 {
     public int Id { get; set; }
-    public Guid StudentClassId { get; set; }
     public int QuizId { get; set; }
+    public GetByIdQuizResponse Quiz { get; set; }
 }
--- src/tobetoFinalProject/Application/Features/ClassQuizs/Queries/GetList/GetListClassQuizQuery.cs
+++ src/tobetoFinalProject/Application/Features/ClassQuizs/Queries/GetList/GetListClassQuizQuery.cs
@@ -8,6 +8,8 @@ using Core.Application.Responses;
 using Core.Persistence.Paging;
 using MediatR;
 using static Application.Features.ClassQuizs.Constants.ClassQuizsOperationClaims;
+using Application.Services.ContextOperations;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Features.ClassQuizs.Queries.GetList;
 
@@ -15,22 +17,27 @@ public class GetListClassQuizQuery : IRequest<GetListResponse<GetListClassQuizLi
 {
     public PageRequest PageRequest { get; set; }
 
-    public string[] Roles => new[] { Admin, Read };
+    public string[] Roles => new[] { Admin, Read,"Student" };
 
     public class GetListClassQuizQueryHandler : IRequestHandler<GetListClassQuizQuery, GetListResponse<GetListClassQuizListItemDto>>
     {
         private readonly IClassQuizRepository _classQuizRepository;
         private readonly IMapper _mapper;
-
-        public GetListClassQuizQueryHandler(IClassQuizRepository classQuizRepository, IMapper mapper)
+        private readonly IContextOperationService _contextOperationService;
+        public GetListClassQuizQueryHandler(IClassQuizRepository classQuizRepository, IMapper mapper, IContextOperationService contextOperationService)
         {
             _classQuizRepository = classQuizRepository;
             _mapper = mapper;
+            _contextOperationService = contextOperationService;
         }
 
         public async Task<GetListResponse<GetListClassQuizListItemDto>> Handle(GetListClassQuizQuery request, CancellationToken cancellationToken)
         {
+            ICollection<Guid> getClassIds = await _contextOperationService.GetStudentClassesFromContext();
             IPaginate<ClassQuiz> classQuizs = await _classQuizRepository.GetListAsync(
+                predicate: cq => getClassIds.Contains(cq.StudentClassId),
+                include: gq => gq.Include(gq => gq.Quiz),
+                
                 index: request.PageRequest.PageIndex,
                 size: request.PageRequest.PageSize, 
                 cancellationToken: cancellationToken
