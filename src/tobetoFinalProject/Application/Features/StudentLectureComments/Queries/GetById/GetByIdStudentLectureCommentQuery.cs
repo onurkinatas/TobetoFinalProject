058c0986d0493b95d21f90b7cb3928677929097f
Koyuncu <130415130+koyuncu5362@users.noreply.github.com>
2024-02-27 17:41:06 +0300

    getbyid comment operations created

--- src/tobetoFinalProject/Application/Features/StudentLectureComments/Queries/GetById/GetByIdStudentLectureCommentQuery.cs
+++ src/tobetoFinalProject/Application/Features/StudentLectureComments/Queries/GetById/GetByIdStudentLectureCommentQuery.cs
@@ -7,6 +7,7 @@ using Core.Application.Pipelines.Authorization;
 using MediatR;
 using static Application.Features.StudentLectureComments.Constants.StudentLectureCommentsOperationClaims;
 using Microsoft.EntityFrameworkCore;
+using Application.Services.ContextOperations;
 
 namespace Application.Features.StudentLectureComments.Queries.GetById;
 
@@ -21,24 +22,31 @@ public class GetByIdStudentLectureCommentQuery : IRequest<GetByIdStudentLectureC
         private readonly IMapper _mapper;
         private readonly IStudentLectureCommentRepository _studentLectureCommentRepository;
         private readonly StudentLectureCommentBusinessRules _studentLectureCommentBusinessRules;
-
-        public GetByIdStudentLectureCommentQueryHandler(IMapper mapper, IStudentLectureCommentRepository studentLectureCommentRepository, StudentLectureCommentBusinessRules studentLectureCommentBusinessRules)
+        private readonly IContextOperationService _contextOperationService;
+        public GetByIdStudentLectureCommentQueryHandler(IMapper mapper, IStudentLectureCommentRepository studentLectureCommentRepository, StudentLectureCommentBusinessRules studentLectureCommentBusinessRules, IContextOperationService contextOperationService)
         {
             _mapper = mapper;
             _studentLectureCommentRepository = studentLectureCommentRepository;
             _studentLectureCommentBusinessRules = studentLectureCommentBusinessRules;
+            _contextOperationService = contextOperationService;
         }
 
         public async Task<GetByIdStudentLectureCommentResponse> Handle(GetByIdStudentLectureCommentQuery request, CancellationToken cancellationToken)
         {
+            Student getStudent = await _contextOperationService.GetStudentFromContext();
+
             StudentLectureComment? studentLectureComment = await _studentLectureCommentRepository.GetAsync(
-                include: slc => slc
-                                  .Include(slc => slc.CommentSubComments),
+                include: slc => slc.Include(slc => slc.Student)
+                                  .ThenInclude(slc => slc.User)
+                                  .Include(slc => slc.CommentSubComments)
+                                  .ThenInclude(csc => csc.Student)
+                                  .ThenInclude(s => s.User),
                 predicate: slc => slc.Id == request.Id, 
                cancellationToken: cancellationToken);
             await _studentLectureCommentBusinessRules.StudentLectureCommentShouldExistWhenSelected(studentLectureComment);
 
             GetByIdStudentLectureCommentResponse response = _mapper.Map<GetByIdStudentLectureCommentResponse>(studentLectureComment);
+            response.CommentSubComments.ToList().ForEach(item => item.IsDeletable = getStudent.Id == item.StudentId);
             return response;
         }
     }
--- src/tobetoFinalProject/Application/Features/StudentLectureComments/Queries/GetById/GetByIdStudentLectureCommentResponse.cs
+++ src/tobetoFinalProject/Application/Features/StudentLectureComments/Queries/GetById/GetByIdStudentLectureCommentResponse.cs
@@ -1,3 +1,4 @@
+using Application.Features.StudentLectureComments.Queries.Dtos;
 using Core.Application.Responses;
 
 namespace Application.Features.StudentLectureComments.Queries.GetById;
@@ -5,8 +6,12 @@ namespace Application.Features.StudentLectureComments.Queries.GetById;
 public class GetByIdStudentLectureCommentResponse : IResponse
 {
     public int Id { get; set; }
-    public Guid LectureId { get; set; }
+    public string ProfilePhotoPath { get; set; }
     public Guid StudentId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Comment { get; set; }
     public ICollection<StudentSubCommentDto>? CommentSubComments { get; set; }
+    public bool IsDeletable { get; set; }
+    public DateTime CreatedDate { get; set; }
 }
